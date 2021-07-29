8d2727d9e5fdd143ef2564379ee998966b65c4c4
Markus-Amano <markus.a.amano@gmail.com>
2021-07-29 00:25:22 -0600

    made better interface

--- jishocli.v
+++ jishocli.v
@@ -3,15 +3,7 @@ module main
 import net.urllib as nu
 import net.http as nh {get}
 import x.json2
-
-fn jisho_url(safequery string) string{
-	return "https://jisho.org/api/v1/search/words?keyword=" + safequery
-}
-
-fn convert_search_to_jisho_url(search string) string {
-	ss := nu.path_escape(search)
-	return jisho_url(ss)
-}
+import os
 
 struct WordReading {
 	mut:
@@ -47,6 +39,20 @@ struct Entry {
 		wordreadings []WordReading
 		senses []Sense
 		attribution Attribution
+}
+
+struct JishoResponse{
+	mut:
+	meta map[string]json2.Any
+	data []Entry
+}
+
+struct JishoWordSearch {
+	base_url string = "https://jisho.org/api/v1/search/words" 
+	mut:
+	keyword string [required]
+	page int = 1
+	response JishoResponse = JishoResponse{}
 }
 
 fn (mut wr WordReading) from_json(f json2.Any) {
@@ -97,7 +103,7 @@ fn (mut ent Entry) from_json(f json2.Any) {
 			'is_common' { ent.is_common = v.bool() }
 			'tags' { ent.tags = v.arr().map(it.str()) }
 			'jlpt' { ent.jlpt = v.arr().map(it.str()) }
-			'wordreadings' {
+			'japanese' {
 				data := v.arr()
 				ent.wordreadings = []WordReading{len: data.len}
 				for mut wr in ent.wordreadings { 
@@ -117,20 +123,56 @@ fn (mut ent Entry) from_json(f json2.Any) {
 	}
 }
 
-fn main() {
-	resp := get(convert_search_to_jisho_url("#jlpt-n1 #verb"))?
+fn (mut jr JishoResponse) from_json(f json2.Any) {
+	obj := f.as_map()
+	for k, v in obj {
+		match k {
+			'meta' { jr.meta = v.as_map() }
+			'data' {
+				data := v.arr()
+				jr.data = []Entry{len: data.len}
+				for mut ent in jr.data { 
+					ent.from_json(data.pop()) 
+				}
+			}
+			else {}
+		}
+	}
+}
+
+fn (jr JishoResponse) is_ok() bool {
+	status := jr.meta["status"].int()// or {error("Error with status code for Jisho Response.")}
+	return status == int(nh.Status.ok)
+}
+
+fn (jws JishoWordSearch) url() string {
+	kw := "?keyword=" + nu.path_escape(jws.keyword)
+	pg := "&page=" + jws.page.str()
+	return jws.base_url + kw + pg 
+}
+
+fn (mut jws JishoWordSearch) search() ? {
+	resp := get(jws.url()) ?
 	jjson := json2.raw_decode(resp.text) ? 
-	println(typeof(jjson).name) 
-	jjson_map := jjson.as_map()
-	data_arry := jjson_map["data"].arr()
-	println(data_arry.len)
-	mut ents := []Entry{len: data_arry.len}
-	
-	for mut ent in ents {
-		data := data_arry.pop()
-		ent.from_json(data)
-		println(ent.slug)
+	jws.response.from_json(jjson)
+}
+
+fn main() {
+
+	if os.args.len>1 {
+		search_string := os.args[1..].join(" ")
+		// println("The arg string is ${}")
+		mut jws := JishoWordSearch{keyword: search_string}
+		// println(jws)
+		jws.search()?
+
+		for ent in jws.response.data {
+			// println("Slug: ${ent.slug} ")
+			println("WordReadings: ${ent.wordreadings} ")
+			println("WordReadings: ${ent.senses} ")
+			println("")
+		}
+
 	}
 
-	println("The number of results is ${ents.len}")
 }
